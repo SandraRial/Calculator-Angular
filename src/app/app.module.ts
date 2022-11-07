7d044813dc92809a82e42aed44714ec5dec7d61a
SandraRial <sandrarialperez@gmail.com>
2022-11-07 14:01:53 +0100

    last arranges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CalculatorComponent } from './calculator/calculator.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ResultadoComponent } from './resultado/resultado.component';
 
 @NgModule({
-  declarations: [AppComponent, CalculatorComponent],
+  declarations: [AppComponent, FormularioComponent, ResultadoComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css'],
+})
+export class FormularioComponent {
+  @Output() resultadoSuma = new EventEmitter<number>();
+
+  deshabilitar = false;
+
+  amountA: number;
+  amountB: number;
+
+  sumar(): void {
+    let resultado = this.amountA + this.amountB;
+    this.resultadoSuma.emit(resultado);
+    // this.result = sum;
+  }
+}
